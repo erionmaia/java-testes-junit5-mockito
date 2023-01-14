5738ce5fed3910c1b412b83446ee9cf2f62a0ff4
Erion Schlenger de Paiva Maia <erionmaia@gmail.com>
2023-01-14 11:30:14 -0300

    Exception treatment

--- src/main/java/br/com/erionmaia/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/erionmaia/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.erionmaia.api.services.impl;
 import br.com.erionmaia.api.domain.User;
 import br.com.erionmaia.api.repositories.UserRepository;
 import br.com.erionmaia.api.services.UserService;
+import br.com.erionmaia.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User finById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
